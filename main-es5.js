ee725c544f7870ddd041910ca9bcfb7ab2d43837
franose371 <franose371@163.com>
2017-08-05 14:25:07 +0800

    fix the es6 compatibility problems and encapsulate functions

--- main-es5.js
+++ main-es5.js
@@ -1,4 +1,6 @@
-(function() {
+"use strict";
+
+(function () {
 	function Mineblock(cloumns, rows) {
 		this.cloumns = cloumns;
 		this.rows = rows;
@@ -10,7 +12,7 @@
 	window.Mineblock = Mineblock;
 })();
 
-(function() {
+(function () {
 	var Mineblock = window.Mineblock;
 	var MineSweeping = {
 		mine_num: 99,
@@ -19,54 +21,55 @@
 		mines: [],
 		cleanArr: [],
 		count: 0,
-		cleanArea: function(columns, rows, mineObject) {
-			for (let k = -1; k < 2; k++) {
-				for (let l = -1; l < 2; l++) {
-					if (columns + l > -1 && rows + k > -1 && columns + l < 30 && rows + k < 16 && mines[columns + l][rows + k].isClicked === false) {
+		cleanArea: function cleanArea(columns, rows, mineObject) {
+			for (var k = -1; k < 2; k++) {
+				for (var l = -1; l < 2; l++) {
+					if (columns + l > -1 && rows + k > -1 && columns + l < 30 && rows + k < 16 && mineObject.mines[columns + l][rows + k].isClicked === false) {
 						mineObject.cleanArr.push({
 							columns: columns + l,
 							rows: rows + k
 						});
-						mines[columns + l][rows + k].isClicked = true;
+						mineObject.mines[columns + l][rows + k].isClicked = true;
 						mineObject.count++;
-						if (mines[columns + l][rows + k].roundMines === 0) {
+						if (mineObject.mines[columns + l][rows + k].roundMines === 0) {
 							mineObject.cleanArea(columns + l, rows + k, mineObject);
 						}
 					}
 				}
 			}
 		},
-		getMineIndex: function(x, y, mineObject) {
-			xIndex = Math.floor(x / mineObject.block_width);
-			yIndex = Math.floor(y / mineObject.block_height);
+		getMineIndex: function getMineIndex(x, y, mineObject) {
+			var xIndex = Math.floor(x / mineObject.block_width);
+			var yIndex = Math.floor(y / mineObject.block_height);
 			return {
 				xIndex: xIndex,
 				yIndex: yIndex
-			}
+			};
 		},
-		getEventPosition: function(ev) {
+		getEventPosition: function getEventPosition(ev) {
 			var x, y;
 			if (ev.layerX || ev.layerX == 0) {
 				x = ev.layerX;
 				y = ev.layerY;
-			} else if (ev.offsetX || ev.offsetX == 0) { // Opera  
+			} else if (ev.offsetX || ev.offsetX == 0) {
+				// Opera  
 				x = ev.offsetX;
 				y = ev.offsetY;
 			}
 			return {
 				x: x,
 				y: y
-			}
+			};
 		},
 		//创建地雷层，设置99个雷
-		createMines: function() {
+		createMines: function createMines() {
 			//创造地雷层的canvas
 			var mineCanvas = document.createElement("canvas");
 			mineCanvas.id = "mineCanvas";
 			mineCanvas.width = '600';
 			mineCanvas.height = '320';
 			mineCanvas.style.border = '1px solid #aaa';
-			mineCanvas.style.position = 'absolute'
+			mineCanvas.style.position = 'absolute';
 			mineCanvas.style.top = 0;
 			mineCanvas.style.left = 0;
 
@@ -82,74 +85,71 @@
 			mineContext.fillStyle = "gray";
 			mineContext.fillRect(0, 0, 600, 320);
 
-
 			var xIndex = [];
 			var yIndex = [];
 			var positions = [];
 			//创建二维数组mines
-			mines = this.mines;
+			var mines = this.mines;
 			mines = new Array(30);
-			for (let i = 0, length1 = mines.length; i < length1; i++) {
+			for (var i = 0, length1 = mines.length; i < length1; i++) {
 				mines[i] = new Array(16);
 			}
 
-			for (let i = 0, length1 = mines.length; i < length1; i++) {
-				for (let j = 0, length1 = mines[i].length; j < length1; j++) {
-					mines[i][j] = new Mineblock(i, j);
+			for (var _i = 0, _length = mines.length; _i < _length; _i++) {
+				for (var j = 0, _length2 = mines[_i].length; j < _length2; j++) {
+					mines[_i][j] = new Mineblock(_i, j);
 				}
 			}
 			//生成所有可能的坐标索引
-			for (let i = 0; i < 30; i++) {
-				for (let j = 0; j < 16; j++) {
+			for (var _i2 = 0; _i2 < 30; _i2++) {
+				for (var _j = 0; _j < 16; _j++) {
 					positions.push({
-						x: i,
-						y: j
+						x: _i2,
+						y: _j
 					});
 				}
 			}
 
 			//在所有可能的坐标中选出99个不重复的坐标
 
-			for (let i = 0; i < 99; i++) {
-				let index = Math.floor(Math.random() * positions.length);
-				let position = positions[index];
-				let x = position.x;
-				let y = position.y;
+			for (var _i3 = 0; _i3 < 99; _i3++) {
+				var _index = Math.floor(Math.random() * positions.length);
+				var position = positions[_index];
+				var x = position.x;
+				var y = position.y;
 				//mines[x][y] = new Mineblock(x, y);
 				mines[x][y].isMined = true;
-				positions.splice(index, 1);
+				positions.splice(_index, 1);
 			}
 			//遍历生成每个格子里的数字
 			var mineNum;
-			for (let i = 0, length1 = mines.length; i < length1; i++) {
-				for (let j = 0, length1 = mines[i].length; j < length1; j++) {
+			for (var _i4 = 0, _length3 = mines.length; _i4 < _length3; _i4++) {
+				for (var _j2 = 0, _length4 = mines[_i4].length; _j2 < _length4; _j2++) {
 					mineNum = 0;
-					for (let k = -1; k < 2; k++) {
-						for (let l = -1; l < 2; l++) {
-							if (i + l > -1 && j + k > -1 && i + l < 30 && j + k < 16) {
-								if (mines[i + l][j + k].isMined === true) {
+					for (var k = -1; k < 2; k++) {
+						for (var l = -1; l < 2; l++) {
+							if (_i4 + l > -1 && _j2 + k > -1 && _i4 + l < 30 && _j2 + k < 16) {
+								if (mines[_i4 + l][_j2 + k].isMined === true) {
 									mineNum++;
 								}
 							}
 						}
 					}
-					mines[i][j].roundMines = mineNum;
+					mines[_i4][_j2].roundMines = mineNum;
 				}
 			}
 
-
-
-			for (let i = 0; i < mines.length; i++) {
-				for (let j = 0; j < mines[i].length; j++) {
-					if (mines[i][j].isMined === true) {
+			for (var _i5 = 0; _i5 < mines.length; _i5++) {
+				for (var _j3 = 0; _j3 < mines[_i5].length; _j3++) {
+					if (mines[_i5][_j3].isMined === true) {
 						mineContext.fillStyle = "red";
-						mineContext.fillRect(mines[i][j].cloumns * 20, mines[i][j].rows * 20, this.block_width, this.block_height); //绘制矩形
+						mineContext.fillRect(mines[_i5][_j3].cloumns * 20, mines[_i5][_j3].rows * 20, this.block_width, this.block_height); //绘制矩形
 					} else {
 						mineContext.fillStyle = "white";
 						mineContext.font = "15px Arial";
 						mineContext.textAlign = 'center';
-						if (mines[i][j].roundMines !== 0) {
-							mineContext.fillText(mines[i][j].roundMines, mines[i][j].cloumns * 20 + 10, mines[i][j].rows * 20 + 15);
+						if (mines[_i5][_j3].roundMines !== 0) {
+							mineContext.fillText(mines[_i5][_j3].roundMines, mines[_i5][_j3].cloumns * 20 + 10, mines[_i5][_j3].rows * 20 + 15);
 						}
 					}
 				}
@@ -157,16 +157,16 @@
 			this.mines = mines;
 		},
 
-		createMask: function() {
-			const BLOCK_WIDTH = this.block_width;
-			const BLOCK_HEIGHT = this.block_height;
+		createMask: function createMask() {
+			var BLOCK_WIDTH = this.block_width;
+			var BLOCK_HEIGHT = this.block_height;
 			//创造遮罩层的canvas
 			var maskCanvas = document.createElement("canvas");
 			maskCanvas.id = "maskCanvas";
 			maskCanvas.width = '600';
 			maskCanvas.height = '320';
 			maskCanvas.style.border = '1px solid #aaa';
-			maskCanvas.style.position = 'absolute'
+			maskCanvas.style.position = 'absolute';
 			maskCanvas.style.top = 0;
 			maskCanvas.style.left = 0;
 
@@ -179,12 +179,12 @@
 			}
 
 			maskContext.fillStyle = "black";
-			maskContext.strokeStyle = 'gray'
+			maskContext.strokeStyle = 'gray';
 			maskContext.lineWidth = 1; //边框宽度
-			for (let x = 0; x < 600; x = x + BLOCK_WIDTH) {
-				for (let y = 0; y < 320; y = y + BLOCK_HEIGHT) {
+			for (var x = 0; x < 600; x = x + BLOCK_WIDTH) {
+				for (var y = 0; y < 320; y = y + BLOCK_HEIGHT) {
 					maskContext.fillRect(x, y, BLOCK_WIDTH, BLOCK_HEIGHT);
-					maskContext.strokeRect(x, y, BLOCK_WIDTH, BLOCK_HEIGHT)
+					maskContext.strokeRect(x, y, BLOCK_WIDTH, BLOCK_HEIGHT);
 				}
 			}
 
@@ -192,47 +192,43 @@
 			var getMineIndex = this.getMineIndex;
 			var cleanArr = this.cleanArr;
 			var selfMine = this;
-			maskCanvas.addEventListener("click", function(e) {
-				p = getPosition(e);
-				index = getMineIndex(p.x, p.y, selfMine);
+			var mines = this.mines;
+			maskCanvas.addEventListener("click", function (e) {
+				var p = getPosition(e);
+				var index = getMineIndex(p.x, p.y, selfMine);
 				maskContext.clearRect(index.xIndex * BLOCK_WIDTH, index.yIndex * BLOCK_HEIGHT, BLOCK_WIDTH - 1, BLOCK_HEIGHT - 1);
 				if (mines[index.xIndex][index.yIndex].isClicked === false) {
 					mines[index.xIndex][index.yIndex].isClicked = true;
 					selfMine.count++;
 				}
 
-				if (selfMine.count === (480 - 99)) {
+				if (selfMine.count === 480 - 99) {
 					maskContext.clearRect(0, 0, 600, 320);
 					alert("you win");
 				}
 				if (mines[index.xIndex][index.yIndex].isMined === true) {
 					maskContext.clearRect(0, 0, 600, 320);
 					alert("you lose");
-					if (confirm("do you want to play again?"))
-
-					{
+					if (confirm("do you want to play again?")) {
 						selfMine.init();
-					} else
-
-					{
+					} else {
 						alert("goodbye");
 					}
 				} else if (mines[index.xIndex][index.yIndex].roundMines === 0 && mines[index.xIndex][index.yIndex].isMined === false) {
 					selfMine.cleanArea(index.xIndex, index.yIndex, selfMine);
 					for (var i = 0, length1 = cleanArr.length; i < length1; i++) {
-						maskContext.clearRect(cleanArr[i].columns * BLOCK_WIDTH, cleanArr[i].rows * BLOCK_HEIGHT, BLOCK_WIDTH - 1, BLOCK_HEIGHT - 1)
+						maskContext.clearRect(cleanArr[i].columns * BLOCK_WIDTH, cleanArr[i].rows * BLOCK_HEIGHT, BLOCK_WIDTH - 1, BLOCK_HEIGHT - 1);
 					}
 				}
 			}, false);
 		},
 
-
-		init: function() {
+		init: function init() {
 			if (document.getElementById("maskCanvas")) {
-				document.getElementById("maskCanvas").remove()
+				document.getElementById("maskCanvas").remove();
 			}
 			if (document.getElementById("mineCanvas")) {
-				document.getElementById("mineCanvas").remove()
+				document.getElementById("mineCanvas").remove();
 			}
 			this.mines = [];
 			this.cleanArr = [];
@@ -241,9 +237,6 @@
 			this.createMask();
 		}
 
-
-
 	};
 	window.MineSweeping = MineSweeping;
 })();
-
